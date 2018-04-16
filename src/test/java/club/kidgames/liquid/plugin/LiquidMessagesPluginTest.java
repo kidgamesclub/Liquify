3d224c112e13fd947db22ae02d31d76a97d2c6f1
BuildTools <unconfigured@null.spigotmc.org>
2018-04-16 09:33:03 -0700

    Adding test

---
+++ src/test/java/club/kidgames/liquid/plugin/LiquidMessagesPluginTest.java
@@ -0,0 +1,14 @@
+package club.kidgames.liquid.plugin;
+
+import org.junit.Test;
+
+/**
+ * For some reason it's
+ */
+public class LiquidMessagesPluginTest {
+
+  @Test
+  public void testSomething() {
+
+  }
+}
